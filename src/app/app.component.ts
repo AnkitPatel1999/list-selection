f8c2b3153fbd7526944119b5398d6bd6008f31a1
AJTech1968 <40666123+AJTech1968@users.noreply.github.com>
2020-09-30 13:50:14 +0530

    initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,81 @@
-import { Component } from '@angular/core';
+import { listA, listB } from './model/data';
+import { Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'list-selection';
+
+export class AppComponent implements OnInit {
+  listA: listA[];
+  listB: listB[];
+  textA;
+  selected;
+  valueCheck =  0;
+
+  constructor() {
+
+    this.listA = [
+      {
+        optionValue : 'hello',
+      },
+      {
+        optionValue : 'world',
+      },
+      {
+        optionValue : 'testing',
+      }
+    ];
+    this.listB = [];
+
+   
+  }
+  ngOnInit() {
+  }
+
+  addA() {
+    const newList: listA = {
+      optionValue: this.textA,
+    };
+    this.listA.push(newList);
+    
+  }
+
+  addToB() {
+    const newListB: listB = {
+      optionValue: this.selected,
+    };
+    this.listB.push(newListB);
+    this.listB.sort((a, b) => (a.optionValue < b.optionValue ? -1 : 1));
+
+    let removeIndex = this.listA.map(function(item) { return item.optionValue; }).indexOf(this.selected);
+    this.listA.splice(removeIndex, 1);
+  }
+
+  onOptionSelected(event) {
+    this.selected = event.optionValue;
+    console.log(this.selected);
+
+  }
+
+  addAllintoB() {
+
+   this.listA.sort((a, b) => (a.optionValue < b.optionValue ? -1 : 1));
+   this.listA.forEach(element => {
+      const newListA: listA = {
+        optionValue: element.optionValue,
+      };
+      console.log(element.optionValue);
+      this.listB.push(newListA);
+    });
+   this.listA.splice(0, this.listA.length);
+  }
+
+  moveUp() {
+
+    
+
+  }
 }
+
---
+++ src/app/model/data.ts
@@ -0,0 +1,7 @@
+export interface listA {
+    optionValue: string;
+}
+
+export interface listB {
+    optionValue: string;
+}
